e1128be7f37ee37c090030c1ac3e9c1aa19caa97
Sirxnine <monterolaleo23@gmail.com>
2025-12-20 19:51:27 -0400

    ajuste de cositas

--- vite-project/src/componentes/CartasIniciales.tsx
+++ vite-project/src/componentes/CartasIniciales.tsx
@@ -9,7 +9,7 @@ export const cartasGOH: Carta[] = [
     tipo: "Luchador",
     rareza: "SS",
     poder: 9800,
-    defensa: 9000,
+    defensa: 9800,
     habilidadPasiva: "Renovación - Se cura automáticamente cuando su salud está baja",
     habilidadUltimate: "Jeabongchim x250.000 - Aumenta su poder 250,000 veces por 3 turnos",
     anime: "GOH"
@@ -56,20 +56,75 @@ export const cartasGOH: Carta[] = [
 ];
 export const cartasNaruto: Carta[] = [
   {
-    id: 1,
+    id: 101,
     nombre: "Naruto Uzumaki",
     imagen: "https://preview.redd.it/reasons-why-naruto-uzumaki-is-the-best-hokage-v0-vq9pms038bya1.jpg?width=640&crop=smart&auto=webp&s=12150642ae4b9cb97122ecf1b2e885db5a9c260f",
     descripcion: "Ex-Hokage de la Hoja.",
-    tipo: "Luchador",
+    tipo: "Ninja",
     rareza: "SS",
     poder: 9800,
     defensa: 9000,
     habilidadPasiva: "Chakra del Kyuubi - Regeneración acelerada",
     habilidadUltimate: "Modo Sabio - Multiplica por 10 todos sus atributos",
     anime: "Naruto Shippuden"
+  },
+  {
+    id: 102,
+    nombre: "Sasuke Uchiha",
+    imagen: "https://i.pinimg.com/736x/41/a5/a8/41a5a88487ce52ec3901f8721187c3a5.jpg",
+    descripcion: "Último sobreviviente del Clan Uchiha.",
+    tipo: "Ninja",
+    rareza: "SS",
+    poder: 9200,
+    defensa: 8200,
+    habilidadPasiva: "Sharingan - Copia técnicas y predice movimientos",
+    habilidadUltimate: "Susano'o Perfecto - Armadura de chakra invulnerable",
+    anime: "Naruto Shippuden"
+  },
+  {
+    id: 103,
+    nombre: "Minato Namikaze",
+    imagen: "https://wallpapers.com/images/hd/minatonamikaze-reanimado-ij3gxuijz1ec0iin.jpg",
+    descripcion: "Cuarto Hokage, conocido como 'Relámpago Amarillo de Konoha'. Padre de Naruto.",
+    tipo: "Ninja",
+    rareza: "S",
+    poder: 8900,
+    defensa: 8700,
+    habilidadPasiva: "Teleportación Instantánea - Se mueve a velocidad relámpago",
+    habilidadUltimate: "Rasengan Volátil - Esfera de chakra de máxima potencia",
+    anime: "Naruto Shippuden"
+  },
+  {
+    id: 104,
+    nombre: "Madara Uchiha",
+    imagen: "https://u-mercari-images.mercdn.net/photos/m38030209152_1.jpg",
+    descripcion: "Fundador de Konoha junto a Hashirama. Busca crear un mundo de ilusiones perfectas.",
+    tipo: "Ninja",
+    rareza: "SS",
+    poder: 9800,
+    defensa: 9200,
+    habilidadPasiva: "Rinnegan Eternal - Ve chakra y controla los seis caminos",
+    habilidadUltimate: "Meteoritos del Cielo - Invoca meteoritos gigantes desde la atmósfera",
+    anime: "Naruto Shippuden"
   }
 ]
-export const cartasEjemplo: Carta[] = [...cartasGOH, ...cartasNaruto];
+export const cartasSoloLeveling:Carta[] = [
+  {
+    id: 201,
+    nombre: "Sung Jin Woo",
+    imagen: "https://preview.redd.it/sung-jin-woo-edit-by-me-v0-67iuvihc30fc1.png?width=640&crop=smart&auto=webp&s=211aa45810ea39339055ffcc70befaf9d32cb048",
+    descripcion: "Monarca de las Sombras",
+    tipo: "Monarca",
+    rareza: "SS",
+    poder: 9800,
+    defensa: 9800,
+    habilidadPasiva: "Sistema de Monarca - Puede aumentar de forma constante sus atributos",
+    habilidadUltimate: "Sombras - Manifiesta un ejercito de mas de 1000 soldados sombra",
+    anime: "Solo Leveling"
+  }
+]
+
+export const cartasEjemplo: Carta[] = [...cartasGOH, ...cartasNaruto, ...cartasSoloLeveling];
 export const gruposAnime: GrupoAnime[] = [
   {
     id: "GOH",
@@ -84,5 +139,12 @@ export const gruposAnime: GrupoAnime[] = [
     imagenPortada: "https://i.pinimg.com/736x/b1/2b/97/b12b9782aa863b75cdbee3a35ab62bd2.jpg",
     descripcion: "Historia de un ninja adolescente que busca ser reconocido como Hokage.",
     cantidadCartas: cartasNaruto.length
+  },
+  {
+    id: "Solo Leveling",
+    nombre: "Solo Leveling",
+    imagenPortada: "https://pendulo.com/imagenes_grandes/9798400/979840090193.GIF",
+    descripcion: "Un mundo donde aparecen portales llenos de monstruos, obligando a humanos con poderes (cazadores) a luchar para proteger a la humanidad",
+    cantidadCartas: cartasSoloLeveling.length
   }
 ];
